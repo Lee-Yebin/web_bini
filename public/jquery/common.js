4b149d5f365ae0040721b9ac5f5a91646930e1d4
yebin lee <84556082+Lee-Yebin@users.noreply.github.com>
2023-09-28 15:47:59 +0900

    0928_1

--- public/jquery/common.js
+++ public/jquery/common.js
@@ -8,42 +8,50 @@ $(function(){
             $('#header').removeClass('nav_scroll');
         }
     });
+
+    // mobile menu
+    var backdropFilterState = 0; 
+    $('.menu-icon').click(function () {
+        $('body').toggleClass('menu-open');
+
+        if (backdropFilterState === 0) {
+            $('#header.nav_scroll').css('backdrop-filter', 'none');
+            backdropFilterState = 1;
+        } else {
+            $('#header.nav_scroll').css('backdrop-filter', 'blur(20px)');
+            backdropFilterState = 0;
+        }
+    });
     
-// dark mode
-const isDarkModeEnabled = localStorage.getItem('darkModeEnabled');
-if (isDarkModeEnabled === 'true') {
-    enableDarkMode();
-}
+    // dark mode
+    const isDarkModeEnabled = localStorage.getItem('darkModeEnabled');
+    if (isDarkModeEnabled === 'true') {
+        enableDarkMode();
+    }
 
-function enableDarkMode() {
-    $('head').append('<link rel="stylesheet" type="text/css" href="../css/dark.css" id="modeStylesheet">');
-    $('.darkMode').hide();
-    $('.nightMode').show();
-    // 이미지 클릭 시 dark 모드 아이콘으로 변경
-    $('.profile > .h_menu_li_img').click(function () {
-        $(this).attr('src', 'https://lee-yebin.github.io/bini/images/memo_icon_dark.svg');
+    function enableDarkMode() {
+        $('head').append('<link rel="stylesheet" type="text/css" href="https://lee-yebin.github.io/bini/public/css/dark.css" id="modeStylesheet">');
+        $('.darkMode').hide();
+        $('.nightMode').show();
+        $('.profile > .h_menu_li_img').attr('src', 'https://lee-yebin.github.io/bini/public/images/memo_icon_dark.svg');
+    }
+    
+    function disableDarkMode() {
+        $('#modeStylesheet').remove();
+        $('.nightMode').hide();
+        $('.darkMode').show();
+        $('.profile > .h_menu_li_img').attr('src', 'https://lee-yebin.github.io/bini/public/images/memo_icon.svg');
+    }
+
+    $('.darkMode').click(function () {
+        enableDarkMode();
+        localStorage.setItem('darkModeEnabled', 'true');
     });
-}
 
-function disableDarkMode() {
-    $('#modeStylesheet').remove();
-    $('.nightMode').hide();
-    $('.darkMode').show();
-    // 이미지 클릭 시 기본 모드 아이콘으로 변경
-    $('.profile > .h_menu_li_img').click(function () {
-        $(this).attr('src', 'https://lee-yebin.github.io/bini/public/images/memo_icon.svg');
+    $('.nightMode').click(function () {
+        disableDarkMode();
+        localStorage.setItem('darkModeEnabled', 'false');
     });
-}
-
-$('.darkMode').click(function () {
-    enableDarkMode();
-    localStorage.setItem('darkModeEnabled', 'true');
-});
-
-$('.nightMode').click(function () {
-    disableDarkMode();
-    localStorage.setItem('darkModeEnabled', 'false');
-});
 
 });
 
